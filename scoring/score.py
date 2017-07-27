18bb692fec92053ff5875049b06fa1352a85da9f
Alistair Lynn <arplynn@gmail.com>
2017-07-27 11:05:01 +0100

    Add tests for the TCR event-based scoring

--- scoring/score.py
+++ scoring/score.py
@@ -8,7 +8,31 @@ class Scorer(object):
         self._arena_data = arena_data
 
     def calculate_scores(self):
-        raise NotImplementedError()
+        return {
+            tla: self.calculate_game_points(info['events'])
+            for tla, info in self._teams_data.items()
+        }
+
+    @staticmethod
+    def calculate_game_points(events):
+        event_handlers = {
+            'b': lambda backs, tokens, score: (backs + 1, tokens, score),
+            'c': lambda backs, tokens, score:
+                (0, tokens, score + 1 + tokens)
+                if backs == 0
+                else (backs - 1, tokens, score),
+            'u': lambda backs, tokens, score: (backs, tokens + 1, score),
+            'd': lambda backs, tokens, score: (backs, tokens - 1, score),
+            'END': lambda backs, tokens, score: (backs, tokens, score + tokens),
+        }
+
+        events = list(events.lower()) + ['END']
+
+        backs, tokens, score = 0, 0, 0
+        for event in events:
+            backs, tokens, score = event_handlers[event](backs, tokens, score)
+
+        return score
 
 
 if __name__ == '__main__':
